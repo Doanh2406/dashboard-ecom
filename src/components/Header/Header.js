80675892faa26474f6bc31b95a2115bdced131c8
Doanh2406 <lxqdoanh.17it1@vku.udn.vn>
2021-08-15 15:59:09 +0700

    updaate header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Header.scss";
+import SearchIcon from "@material-ui/icons/Search";
+import Badge from "@material-ui/core/Badge";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+
+
+function Header() {
+  return (
+   
+    <div className="header">
+      <div className="header__name"> OverView</div>
+      <div className="header__search">
+        <SearchIcon className="header__search-icon"/>
+        <input
+          type="text"
+          placeholder="Search ..."
+          className="header__search-box"
+        />
+      </div>
+      <div className="header__bage-icon">
+        <Badge badgeContent={4} color="primary" >
+          <NotificationsNoneIcon className="header__bage-icon__item" />
+        </Badge>
+        <Badge badgeContent={4}  color="primary">
+          <AddShoppingCartIcon  className="header__bage-icon__item" />
+        </Badge>
+      </div>
+      <div className="header__button">
+            <AddCircleOutlineIcon className="header__button-icon"/>
+          <span className="header__button-tittle" >Add Product</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
 
 export default function Dashboard() {
   return (
-    <div>
-      Dashboard
+    <div className="dashboard">
+      <div className="dashboard__top">
+        
+      </div>
+      <div className="dashboard__mid">
+
+      </div>
+      <div className="dashboard__bottom">
+
+      </div>
     </div>
   )
 }
