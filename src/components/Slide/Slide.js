74c98f2a0bebe0e55a8b5e02784b6c229bb62bdc
Doanh2406 <Doanh2406.github@.com>
2021-09-03 19:10:50 +0700

    slide product img

---
+++ src/components/Slide/Slide.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import "./Slide.scss";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+const data = [
+  "https://vetra.laborasyon.com/assets/images/products/5.jpg",
+  "https://vetra.laborasyon.com/assets/images/products/2.jpg",
+  "https://vetra.laborasyon.com/assets/images/products/1.jpg",
+  "https://vetra.laborasyon.com/assets/images/products/4.jpg",
+  "https://vetra.laborasyon.com/assets/images/products/3.jpg",
+  "https://vetra.laborasyon.com/assets/images/products/5.jpg",
+];
+function Slide() {
+  const [selectImg, setSelectImg] = useState(data[0]);
+  const [currentIndex, setCurrentIndex] = useState(1);
+  // const [currentLeft, setCurrentLeft] = useState(0);
+  // const [currentRight, setCurrentRight] = useState(3);
+
+  const handleBack = (select) => {
+    let vardump = data.findIndex((x) => x === select);
+    if (vardump - 1 > -1) {
+      setSelectImg(data[vardump - 1]);
+      setCurrentIndex(vardump-1)
+    } else {
+      vardump = data.length;
+
+      setSelectImg(data[vardump]);
+      setCurrentIndex(vardump)
+    }
+    console.log(vardump);
+  };
+
+  const handleForward = (select) => {
+    let vardump = data.findIndex((x) => x === select);
+    console.log(vardump)
+    if (vardump + 1 < data.length) {
+      setSelectImg(data[vardump + 1]);
+      setCurrentIndex(vardump+1)
+    } else {
+      vardump = 0;
+      setSelectImg(data[vardump]);
+      setCurrentIndex(vardump)
+    }
+    console.log(vardump);
+  };
+
+  const handleCurent = (item) => {
+    setSelectImg(item);
+    // console.log(currentIndex);
+    let indexImge = data.findIndex((x) => x === item);
+    if (indexImge > 0 && indexImge < data.length) {
+      setCurrentIndex(indexImge);
+    } else {setCurrentIndex(1);
+    }
+  };
+
+  return (
+    <div className="slide-image">
+      <div className="big-image">
+        <div className="big-image__item">
+          <div
+            className="big-image__icon"
+            onClick={() => handleBack(selectImg)}
+          >
+            <ArrowBackIosIcon />
+          </div>
+          <img className="big-image__img" src={selectImg}></img>
+          <div
+            className="big-image__icon"
+            onClick={() => handleForward(selectImg)}
+          >
+            <ArrowForwardIosIcon />
+          </div>
+        </div>
+      </div>
+      <div className="small-image">
+        <div className="big-image__icon" onClick={() => handleBack(selectImg)}>
+          <ArrowBackIosIcon />
+        </div>
+        <div className="small-image__item">
+          {data.slice(currentIndex - 1, currentIndex + 2).map((img, index) => (
+            <img
+              key={index}
+              style={{ border: selectImg === img ? "4px solid purple" : "" }}
+              className="small-image__img"
+              src={img}
+              onClick={() => handleCurent(img)}
+            ></img>
+          ))}
+        </div>
+        <div
+          className="big-image__icon"
+          onClick={() => handleForward(selectImg)}
+        >
+          <ArrowForwardIosIcon />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Slide;
--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -6,50 +6,89 @@ import LocalMallIcon from "@material-ui/icons/LocalMall";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import "./Dashboard.scss";
 import Header from "../../Header/Header";
+import Slide from "../../Slide/Slide";
 export default function Dashboard() {
   return (
     <div className="dashboard">
-      <Header/>
+      <Header />
       <div className="dashboard__top">
         <div className="dashboard__top-left">
           <div className="dashboard__top-left__title">
             <span className="dashboard__top-left__title-text">Sales Chart</span>
-            <HelpOutlineIcon  color="primary" />
+            <HelpOutlineIcon color="primary" />
           </div>
           <div className="dashboard__top-left__layout">
             <div className="dashboard__top-left__layout-infor">
               <div className="dashboard__top-left__layout-text">
-                <LocalMallIcon style={{ fontSize: "36px",marginRight:"10px", color:"#05B171" }} />
+                <LocalMallIcon
+                  style={{
+                    fontSize: "36px",
+                    marginRight: "10px",
+                    color: "#05B171",
+                  }}
+                />
                 <span>$10.552,40</span>
                 <div className="layout-text__recent">
-                  <ArrowUpwardIcon style={{ fontSize: "30px",marginLeft:"10px", color:"#05B171" }} />
+                  <ArrowUpwardIcon
+                    style={{
+                      fontSize: "30px",
+                      marginLeft: "10px",
+                      color: "#05B171",
+                    }}
+                  />
                   <span>8.3%</span>
                 </div>
               </div>
               <div className="dashboard__layout-selects">
-                <select  className="dashboard__layout-select">
-                  <option className="dashboard__layout-select__item" value="0">January</option>
-                  <option className="dashboard__layout-select__item" value="1">Febuary</option>
-                  <option className="dashboard__layout-select__item" value="2">March</option>
-                  <option className="dashboard__layout-select__item" value="3">April</option>
-                  <option className="dashboard__layout-select__item" value="5">June</option>
-                  <option className="dashboard__layout-select__item" value="6">July</option>
-                  <option className="dashboard__layout-select__item" value="7">August</option>
-                  <option className="dashboard__layout-select__item" value="8">September</option>
-                  <option className="dashboard__layout-select__item" value="9">October</option>
-                  <option className="dashboard__layout-select__item" value="10">November</option>
-                  <option className="dashboard__layout-select__item" value="11">December</option>
+                <select className="dashboard__layout-select">
+                  <option className="dashboard__layout-select__item" value="0">
+                    January
+                  </option>
+                  <option className="dashboard__layout-select__item" value="1">
+                    Febuary
+                  </option>
+                  <option className="dashboard__layout-select__item" value="2">
+                    March
+                  </option>
+                  <option className="dashboard__layout-select__item" value="3">
+                    April
+                  </option>
+                  <option className="dashboard__layout-select__item" value="5">
+                    June
+                  </option>
+                  <option className="dashboard__layout-select__item" value="6">
+                    July
+                  </option>
+                  <option className="dashboard__layout-select__item" value="7">
+                    August
+                  </option>
+                  <option className="dashboard__layout-select__item" value="8">
+                    September
+                  </option>
+                  <option className="dashboard__layout-select__item" value="9">
+                    October
+                  </option>
+                  <option className="dashboard__layout-select__item" value="10">
+                    November
+                  </option>
+                  <option className="dashboard__layout-select__item" value="11">
+                    December
+                  </option>
                   {/* <option value="12">Volvo</option> */}
                 </select>
               </div>
             </div>
-            <LineChart />
+            {/* <LineChart /> */}
+            <Slide/>
           </div>
         </div>
         <div className="dashboard__top-right">
-        <div className="dashboard__top-left__title">
+          <div className="dashboard__top-left__title">
             <span className="dashboard__top-left__title-text">Channel</span>
-            <HelpOutlineIcon  color="primary" />
+            <HelpOutlineIcon color="primary" />
+          </div>
+          <div className="contenr-rights">
+            
           </div>
         </div>
       </div>
