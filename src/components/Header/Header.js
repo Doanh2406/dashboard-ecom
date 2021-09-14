3c1bb7ad7de3f9ba9ac00bbb73f1c686b6c9ebe6
t3n1o12 <lvnhat.17it1@vku.udn.vn>
2021-09-14 16:22:15 +0700

    asd

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,15 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getCart } from "../redux/actions/cartActions";
+import { detailProduct } from "../redux/actions/productActions";
 import TabsInfor from "../Tabs/TabsInfor";
 import "./Header.scss";
 
 function Header() {
   const [infor, setInfor] = useState(false);
+  const { product } = useSelector(state => state.productSearch)
+  const { list } = useSelector(state =>state.userListSearch)
+  const [search, setSearch] = useState()
   function handleBoxClick() {
     setInfor(!infor);
   }
@@ -33,6 +37,52 @@ function Header() {
           placeholder="Search ..."
           className="header__search-box"
         />
+         {
+              product ? <div style={product===undefined?{display:'none',}:null} className='search_container_form'>
+                {
+                  product && product.map(item => {
+                    return (
+                      <NavLink onClick={()=>dispatch(detailProduct(item._id))} to={`/shopping/${item._id}`} style={{ textDecoration: 'none' }}>
+                        <div className='search_container_item' >
+
+                          <img src={'http://localhost:5000/upload/product/' + item.image[0].filename} alt='' />
+                          <div className='search_text'>
+                            <p style={{ fontSize: 14 }} >Name: {item.name}</p>
+                            <p style={{ marginTop: -12 }}>Category: {item.category}</p>
+                            <p style={{ marginTop: -10 }}>Price: {'$' + item.price}</p>
+                            {item.sale && <p style={{ marginTop: -10 }}>Sale: {'$' + item.sale}</p>}
+                          </div>
+
+                        </div>
+                      </NavLink>)
+                  })
+
+                }
+              </div> : null
+            }
+              {
+              list ? <div style={{width:'17%'}}  className='search_container_form'>
+                {
+                  list && list.map(item => {
+                    return (
+                      <NavLink onClick={()=>dispatch(detailProduct(item._id))} to={`/shopping/${item._id}`} style={{ textDecoration: 'none' }}>
+                        <div  className='search_container_item' >
+
+                          <img src={'http://localhost:5000/' + item.userAva} alt='' />
+                          <div className='search_text'>
+                            <p style={{ fontSize: 14 }} >Name: {item.name}</p>
+                            <p style={{ marginTop: -12 }}>Email: {item.email}</p>
+                            <p style={{ marginTop: -10 }}>Country: { item.country?item.country:'Not set yet'}</p>
+                    
+                          </div>
+
+                        </div>
+                      </NavLink>)
+                  })
+
+                }
+              </div> : null
+            }
       </div>
       <div className="header__bage-icon">
         <Badge badgeContent={4} color="error" onClick={handleBoxClick}>
