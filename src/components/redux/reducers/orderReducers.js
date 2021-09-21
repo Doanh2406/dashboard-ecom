b6c586fb6656156cb30ad2cdf8655a99f6f3cd84
t3n1o12 <lvnhat.17it1@vku.udn.vn>
2021-09-21 20:27:07 +0700

    ÁD

--- src/components/redux/reducers/orderReducers.js
+++ src/components/redux/reducers/orderReducers.js
@@ -1,4 +1,4 @@
-import { ORDER_LIST_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS } from "../constants/orderConstants";
+import { ORDER_DETAIL_FAIL, ORDER_DETAIL_REQUEST, ORDER_DETAIL_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS } from "../constants/orderConstants";
 
 export const orderListReducer = (state={}, action)=>{
     switch (action.type) {
@@ -12,6 +12,27 @@ export const orderListReducer = (state={}, action)=>{
                 list:action.payload
             }
         case ORDER_LIST_FAIL:
+            return{
+                loading:false,
+                error:action.payload
+            }
+        default:
+            return state;
+    }
+}
+
+export const orderDetailReducer = (state={}, action)=>{
+    switch (action.type) {
+        case ORDER_DETAIL_REQUEST:
+            return{
+                loading:true
+            }
+        case ORDER_DETAIL_SUCCESS:
+            return{
+                loading:false,
+                order:action.payload
+            }
+        case ORDER_DETAIL_FAIL:
             return{
                 loading:false,
                 error:action.payload
